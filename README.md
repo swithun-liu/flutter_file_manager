2ae2347ebc7da1b87d1984801b9e79ddf0113dd6
lancerXXXX <2571021108.@qq.com>
2020-03-18 15:07:48 +0800

    添加bottomBar

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # flutter_file_manager
 
 - 一个 双栏 Flutter 文件管理器
+- 左边显示上一级目录, 右边显示下一级目录
 - 目前只能打开文件,
 
 <img src="assets/images/安卓手机示例.jpg" style="zoom:30%;" />
--- lib/file_manager.dart
+++ lib/file_manager.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path/path.dart' as p;
@@ -19,49 +21,35 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHome> {
-  //文件,目录和链接都继承自FileSystemEntity
-  //FileSystemEntityType有三个常量：
-  //                               Directory、FILE、LINK、NOT_FOUND
-  //FileSystemEntity.isFile .isLink .isDerectory可用于判断类型
-  List<FileSystemEntity> leftFiles = [];   //左列的文件
-  List<FileSystemEntity> rightFiles = [];    //右列的文件
+  List<FileSystemEntity> leftFiles = []; //左列的文件
+  List<FileSystemEntity> rightFiles = []; //右列的文件
 
   double iconHight = 40.0;
-  double iconWidth=40.0;
-  double fileFontSize=10.0;
-  double subTitleFontSize=8.0;
-  Directory parentDir;  //父目录
+  double iconWidth = 40.0;
+  double fileFontSize = 10.0;
+  double subTitleFontSize = 8.0;
+  Directory parentDir; //父目录
   ScrollController controller = ScrollController();
   List<double> position = [];
-
-//  final _subdirectorys = <WordPair>[];
-//  final _biggerFont = const TextStyle(fontSize: 10.0);
 
   ///初始化: 拿到根路径 刷新目录
   @override
   void initState() {
     super.initState();
-    parentDir = Directory(Common().sDCardDir);   //获取根路径
-    initPathFiles(Common().sDCardDir,-2);   //刷新左列目录
+    parentDir = Directory(Common().sDCardDir); //获取根路径
+    initPathFiles(Common().sDCardDir, -2); //刷新左列目录
   }
 
   ///用于导航返回拦截器的onWillPop
-  ///也就是再返回的时候用的
-  ///如果再根目录的话再按返回就退出
-  ///如果不是就parentDir.parent.path拿到父目录然后跳转
   Future<bool> onWillPop() async {
-    /// 如果不是根目录,就跳转到上层目录并刷新目录
-    if (parentDir.parent.path!=Common().sDCardDir) {
-      //刷新左边 true
-      initPathFiles(parentDir.path,2);
-//      jumpToPosition(false);
-    }
-
-    ///否则退出
-    else {
+    if (parentDir.parent.path != Common().sDCardDir) {
+      /// 如果不是根目录,就跳转到上层目录并刷新目录
+      initPathFiles(parentDir.path, 2);
+    } else {
+      ///否则退出
       ///https://blog.csdn.net/weixin_33979203/article/details/88019065
       ///Router: 路由 对屏幕界面的抽象 每一个页面都有相应的Page
-      print('退出时parentDir为:'+parentDir.path);
+      print('退出时parentDir为:' + parentDir.path);
       SystemNavigator.pop();
     }
     return false;
@@ -69,8 +57,6 @@ class _MyHomePageState extends State<MyHome> {
 
   @override
   Widget build(BuildContext context) {
-    ///导航返回拦截器: WillPopScope
-    ///连续按两次才退出
     return WillPopScope(
       onWillPop: onWillPop,
       child: Scaffold(
@@ -88,21 +74,33 @@ class _MyHomePageState extends State<MyHome> {
           leading: parentDir?.path == Common().sDCardDir
               ? Container()
               : IconButton(
-            icon: Icon(
-              Icons.chevron_left,
-              color: Colors.black,
-            ),
-            onPressed: onWillPop,
-          ), //再标题前面显示的一个控件
+                  icon: Icon(
+                    Icons.chevron_left,
+                    color: Colors.black,
+                  ),
+                  onPressed: onWillPop,
+                ), //再标题前面显示的一个控件
         ),
-//        body: _fileListView(files.length)
         body: Row(
           children: <Widget>[
             Expanded(
               child: _fileListView(leftFiles, -1),
             ),
             Expanded(
-              child: _fileListView(rightFiles,1),
+              child: _fileListView(rightFiles, 1),
+            )
+          ],
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu),
             )
           ],
         ),
@@ -122,9 +120,9 @@ class _MyHomePageState extends State<MyHome> {
             itemCount: files.length,
             itemBuilder: (BuildContext context, int index) {
               if (FileSystemEntity.isFileSync(files[index].path))
-                return _buildFileItem(files[index],type);
+                return _buildFileItem(files[index], type);
               else
-                return _buildFolderItem(files[index],type);
+                return _buildFolderItem(files[index], type);
             }),
       );
     }
@@ -141,18 +139,23 @@ class _MyHomePageState extends State<MyHome> {
       child: Container(
         decoration: BoxDecoration(
           border:
-          Border(bottom: BorderSide(width: 0.5, color: Color(0xffe5e5e5))),
+              Border(bottom: BorderSide(width: 0.5, color: Color(0xffe5e5e5))),
         ),
         child: ListTile(
-          leading: Image.asset(Common().selectIcon(p.extension(file.path)),height: 40.0,width: 40.0,),
+          leading: Image.asset(
+            Common().selectIcon(p.extension(file.path)),
+            height: 40.0,
+            width: 40.0,
+          ),
 
           ///TODO 解释p.etension
-          title: Text(file.path.substring(file.parent.path.length + 1),style: TextStyle(fontSize: fileFontSize)),
+          title: Text(file.path.substring(file.parent.path.length + 1),
+              style: TextStyle(fontSize: fileFontSize)),
 
           ///从文件路径中截取文件名字 (file.parent.length+1-文件父目录长度)
           subtitle: Text(
             '$modifiledTime ${Common().getFileSize(file.statSync().size)}',
-            style: TextStyle(fontSize:subTitleFontSize),
+            style: TextStyle(fontSize: subTitleFontSize),
           ), //时间和文件大小
         ),
         //TODO onTap()
@@ -161,16 +164,62 @@ class _MyHomePageState extends State<MyHome> {
       onTap: () {
         OpenFile.open(file.path);
       },
+      onLongPress: () {
+        showModalBottomSheet(
+            backgroundColor: Color(0x00000000),
+            context: context,
+            builder: (BuildContext context) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: type == -1
+                    ? CrossAxisAlignment.start
+                    : CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 170,
+                    child: Card(
+                      margin: EdgeInsets.only(
+                          left: 5, right: 5, top: 20, bottom: 20),
+                      color: Colors.white,
+                      child: InkWell(
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text('重命名',
+                                style: TextStyle(color: Colors.blue)),
+                          ),
+                        ),
+                        onTap: () {
+                          renameFile(file, type);
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 170,
+                    child: Card(
+                      margin: EdgeInsets.only(left: 5, right: 5, bottom: 50),
+                      color: Colors.white,
+                      child: InkWell(
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text('删除',
+                                style: TextStyle(color: Colors.blue)),
+                          ),
+                        ),
+                        onTap: () {
+                          deleteFile(file, type);
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              );
+            });
+      },
     );
   }
-
-  /// 创建文件夹的ListTile
-
-//  Future openFile(String path) async {
-//    ///dynamic (不做类型检查)
-//    final Map<String, dynamic> args = <String, dynamic>{'path': path};
-//    await _channel.invokeMethod('openFile', args);
-//  }
 
   Widget _buildFolderItem(FileSystemEntity file, int type) {
     String modifiledTime = DateFormat('yyyy-MM-dd HH:mm:ss', 'zh_CN')
@@ -180,17 +229,20 @@ class _MyHomePageState extends State<MyHome> {
       child: Container(
         decoration: BoxDecoration(
           border:
-          Border(bottom: BorderSide(width: 0.5, color: Color(0xffe5e5e5))),
+              Border(bottom: BorderSide(width: 0.5, color: Color(0xffe5e5e5))),
         ),
         child: ListTile(
-          leading: Image.asset('assets/images/folder.png',height: 40.0,width: 40.0,),
+          leading: Image.asset(
+            'assets/images/folder.png',
+            height: 40.0,
+            width: 40.0,
+          ),
           title: Row(
             children: <Widget>[
               Expanded(
                 child: Text(file.path.substring(file.parent.path.length + 1),
                     style: TextStyle(fontSize: fileFontSize)),
               ),
-              ///TODO 解释这里为什么要用Expanded
             ],
           ),
           subtitle: Text(
@@ -203,14 +255,185 @@ class _MyHomePageState extends State<MyHome> {
         ),
       ),
       onTap: () {
-          initPathFiles(file.path,type);
+        initPathFiles(file.path, type);
       },
-      //TODO onLongPressed
+      onLongPress: () {
+        showModalBottomSheet(
+            backgroundColor: Color(0x00000000),
+            context: context,
+            builder: (BuildContext context) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: type == -1
+                    ? CrossAxisAlignment.start
+                    : CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 170,
+                    child: Card(
+                      margin: EdgeInsets.only(
+                          left: 5, right: 5, top: 20, bottom: 20),
+                      color: Colors.white,
+                      child: InkWell(
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text('重命名',
+                                style: TextStyle(color: Colors.blue)),
+                          ),
+                        ),
+                        onTap: () {
+                          renameFile(file, type);
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 170,
+                    child: Card(
+                      margin: EdgeInsets.only(left: 5, right: 5, bottom: 50),
+                      color: Colors.white,
+                      child: InkWell(
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text('删除',
+                                style: TextStyle(color: Colors.blue)),
+                          ),
+                        ),
+                        onTap: () {
+                          deleteFile(file, type);
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              );
+            });
+      },
     );
   }
 
   //////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////
+//  删除文件
+  void deleteFile(FileSystemEntity file, int type) {
+    showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text('重命名'),
+            content: Text('删除后不可恢复'),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text(
+                  '取消',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              CupertinoDialogAction(
+                child: Text(
+                  '确定',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                onPressed: () {
+                  if (file.statSync().type == FileSystemEntityType.directory) {
+                    Directory directory = Directory(file.path);
+                    directory.deleteSync(recursive: true);
+                  } else if (file.statSync().type ==
+                      FileSystemEntityType.file) {
+                    file.deleteSync();
+                  }
+                  if (type == -1) {
+                    initPathFiles(file.path, -3);
+                  } else {
+                    initPathFiles(file.parent.path, type);
+                  }
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+
+//重命名文件
+  void renameFile(FileSystemEntity file, int type) {
+    TextEditingController _controller = TextEditingController();
+
+    showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Center(
+              child: CupertinoAlertDialog(
+                title: Text('重命名'),
+                content: Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: '请输入新名称',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(width: 0.3)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(width: 0.5)),
+                      contentPadding: EdgeInsets.all(8.0),
+                    ),
+                  ),
+                ),
+                actions: <Widget>[
+                  CupertinoButton(
+                    child: Text(
+                      '取消',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  CupertinoButton(
+                    child: Text(
+                      '确认',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    onPressed: () async {
+                      String newName = _controller.text;
+                      if (newName.trim().length == 0) {
+                        Fluttertoast.showToast(
+                            msg: '名字不能为空', gravity: ToastGravity.BOTTOM);
+                        return;
+                      }
+                      String newPath = "";
+                      if (file.statSync().type == FileSystemEntityType.file) {
+                        newPath = file.parent.path +
+                            '/' +
+                            newName +
+                            p.extension(file.path);
+                      } else {
+                        newPath = file.parent.path + '/' + newName;
+                      }
+                      file.renameSync(newPath);
+                      if (type == -1) {
+                        initPathFiles(newPath, -3);
+                      } else {
+                        initPathFiles(file.parent.path, type);
+                      }
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
 
   ///用传来的路径刷新该目录下的文件.文件夹
   void initPathFiles(String path, int type) {
@@ -218,18 +441,28 @@ class _MyHomePageState extends State<MyHome> {
       //可变的Widget可以使用setState()函数--重新绘制试图--调用build方法--绘制不一样的地方
       setState(() {
         parentDir = Directory(path);
-        print('parentDir 更改为'+parentDir.path);
-        if(type==-1){//刷新右列表
-          sortFile(parentDir,-1);
-        }else if(type==1){//往右走 先刷右列表 再刷左列表
+        print('parentDir 更改为' + parentDir.path);
+        if (type == -1) {
+          //点击左列
+          //刷新右列表
+          sortFile(parentDir, -1);
+        } else if (type == 1) {
+          //点击右列
+          //往右走 先刷右列表 再刷左列表
           sortFile(parentDir, -1);
           sortFile(parentDir.parent, 1);
-        }else if(type==-2){
-          sortFile(parentDir,1);
-        }else if(type==2){
+        } else if (type == -2) {
+          //初始化
+          sortFile(parentDir, 1);
+        } else if (type == 2) {
+          //返回时
           sortFile(parentDir.parent, -1);
           sortFile(parentDir.parent.parent, 1);
-          parentDir=parentDir.parent;
+          parentDir = parentDir.parent;
+        } else if (type == -3) {
+          //左列重命名文件夹
+          sortFile(parentDir.parent, 1);
+          sortFile(parentDir, -1);
         }
       });
     } catch (e) {
@@ -239,7 +472,7 @@ class _MyHomePageState extends State<MyHome> {
   }
 
   ///目录排序
-  void sortFile(Directory parentDir,int type) {
+  void sortFile(Directory parentDir, int type) {
     List<FileSystemEntity> _Files = [];
     List<FileSystemEntity> _Folders = [];
     //parenDire--路径
@@ -256,12 +489,14 @@ class _MyHomePageState extends State<MyHome> {
     }
     _Files.sort((a, b) => a.path.toLowerCase().compareTo(b.path.toLowerCase()));
     _Folders.sort(
-            (a, b) => a.path.toLowerCase().compareTo(b.path.toLowerCase()));
-    if (type==-1) {//刷新右列
+        (a, b) => a.path.toLowerCase().compareTo(b.path.toLowerCase()));
+    if (type == -1) {
+      //刷新右列
       rightFiles.clear();
       rightFiles.addAll(_Folders);
       rightFiles.addAll(_Files);
-    } else {  //刷新左列
+    } else {
+      //刷新左列
       leftFiles.clear();
       leftFiles.addAll(_Folders);
       leftFiles.addAll(_Files);
@@ -274,27 +509,6 @@ class _MyHomePageState extends State<MyHome> {
       controller.jumpTo(0.0);
     } else {
       try {
-        ///https://www.jianshu.com/p/a4affde4c8ca
-        ///Dart: 单线程模式
-        ///     模型: Event-Looper & Event-Queue
-        ///     依次执行: 通过EventLooper (事件循环)
-        ///               - 取Event (EventQueue)
-        ///               - 处理Event
-        ///               - 直到EventQueue为空
-        ///     :isolate(隔离) ---Dart无线程概念
-        ///                    - isolate相互隔离
-        ///                    - 不会共享内存
-        ///                     程序开始: Main isolate->main()
-        ///                     接着处理EventQueue中的Event(one by one)
-        ///      : Event Queue & Microtask Queue
-        ///             - Main Isolate只有一个Event looper
-        ///                               两个Event Queue: Event Queue & Microtask Queue((优先执行)处理稍晚一些的事情(但是下一个消息来之前要处理完的),处理这个Queue时停止Event Queue的处理)
-        ///异步任务调度:
-        ///             Future: 任务加到Event Queue队尾
-        ///             scheduleMidrotask: 任务加到Microtask Queue队尾(要避免过大--阻塞其他事件的处理)
-        ///Future.then: 大人物拆分成小任务一步步执行
-        ///Future.delayed: 任务延迟执行
-        ///Duration:
         await Future.delayed(Duration(milliseconds: 1));
         // "?" 的作用: 标示对象可以是 null
         controller?.jumpTo(position[position.length - 1]);
